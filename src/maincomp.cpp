c22fa868e546ce14c267e4b283449feb5f41279a
Vasiliy Litvinov <kvas.omsk@gmail.com>
2016-07-23 01:39:07 +0600

    выпиливание ненужного

--- src/maincomp.cpp
+++ src/maincomp.cpp
@@ -6,14 +6,6 @@
 #include "../include/ifc_ldev.h"
 #include "../include/ldevbase.h"
 
-#include "../include/plx.h"
-#include "../include/791.h"
-#include "../include/e440.h"
-#include "../include/e140.h"
-#include "../include/e2010.h"
-#include "../include/e154.h"
-#include "../include/e310.h"
-
 #include <stdio.h>
 
 // add class for new board here.
@@ -32,57 +24,11 @@ extern "C" LUnknown* CreateInstance(ULONG Slot)
 
    pL->GetSlotParam(&sl);
 
-   switch(sl.BoardType)
-   {
-   case PCIA:
-   case PCIB:
-   case PCIC:
-      {
+     /* {
          pI = static_cast<IDaqLDevice*>(new DaqL780(Slot));
          pI->AddRef();
-      } break;
+      } break;*/
 
-   case L791:
-      {
-         pI = static_cast<IDaqLDevice*>(new DaqL791(Slot));
-         pI->AddRef();
-      } break;
-
-   case E440:
-      {
-         pI = static_cast<IDaqLDevice*>(new DaqE440(Slot));
-         pI->AddRef();
-      } break;
-
-   case E140:
-	case 90:
-      {
-         pI = static_cast<IDaqLDevice*>(new DaqE140(Slot));
-         pI->AddRef();
-      } break;
-
-   case E2010B:
-   case E2010:
-      {
-         pI = static_cast<IDaqLDevice*>(new DaqE2010(Slot));
-         pI->AddRef();
-      } break;
-
-   case E154:
-      {
-         pI = static_cast<IDaqLDevice*>(new DaqE154(Slot));
-         pI->AddRef();
-      } break;
-
-   case E310:
-      {
-         pI = static_cast<IDaqLDevice*>(new DaqE310(Slot));
-         pI->AddRef();
-      } break;
-
-   default: { pI=NULL; pL->sal->LSetLastError(L_NOTSUPPORTED); }
-
-   }
 
    pL->CloseLDevice();
    delete pL;
